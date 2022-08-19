7abc732902ed84ca76114fae6132cba6f953be0e
reiok0208 <62207092+reiok0208@users.noreply.github.com>
2022-08-19 14:41:23 +0900

    folder_create_add

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 });
 
 Route::get('/folders/{id}/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+Route::get('/folders/create', [App\Http\Controllers\FolderController::class, 'showCreateForm'])->name('folders.create');
+Route::post('/folders/create', [App\Http\Controllers\FolderController::class, 'create']);
