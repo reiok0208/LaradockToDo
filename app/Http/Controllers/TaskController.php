5aab4765d8d0f5039c183004c4147166fcf66caa
reiok0208 <62207092+reiok0208@users.noreply.github.com>
2022-08-19 13:48:29 +0900

    folder&task_index_add

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Folder;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function index(int $id)
+    {
+        // すべてのフォルダを取得する
+        $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+        ]);
+    }
 }
